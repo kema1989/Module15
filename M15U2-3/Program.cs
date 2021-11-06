0f14d29bf1426a8dda0914bee79a9b77f5fe945c
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-11-06 22:00:39 +0600

    Юниты 1&2✔

---
+++ M15U2-3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace M15U2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ArithmeticMean(2, 7, 9, -7, 6, 3));
+        }
+
+        static double ArithmeticMean(params int[] numbers)
+        {
+            return Math.Round(numbers.Sum() / (double)numbers.Length , 7);
+        }
+    }
+}
---
+++ M15U2-8/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace M15U2_8
+{
+    class Program
+    {
+        public static List<int> list = new List<int>();
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Введите число:");
+
+                    var number = Convert.ToInt32(Console.ReadLine());
+
+                    list.Add(number);
+
+                    Console.WriteLine($"Количество чиел в списке: {list.Count}\nСумма всех чисел: {list.Sum()}");
+                    Console.WriteLine($"Наибольшее число - {list.Max()}, наименьшее - {list.Min()}, среднее - {list.Average()}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Main(args);
+            }
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    while (true)
+        //    {
+        //        // Читаем введенный с консоли  текст
+        //        var input = Console.ReadLine();
+
+        //        // проверяем, число ли это
+        //        var isInteger = Int32.TryParse(input, out int inputNum);
+
+        //        // если не число - показываем ошибку
+        //        if (!isInteger)
+        //        {
+        //            Console.WriteLine();
+        //            Console.WriteLine("Вы ввели не число");
+        //        }
+        //        // если соответствует, запускаем обработчик
+        //        else
+        //        {
+        //            // добавляем в список
+        //            Numbers.Add(inputNum);
+
+        //            // выводим все критерии
+        //            Console.WriteLine("Число " + input + " добавлено в список.");
+        //            Console.WriteLine($&quot; Всего в списке  { Numbers.Count}
+        //            чисел");
+        //            Console.WriteLine($"Сумма:  {Numbers.Sum()}");
+        //            Console.WriteLine($"Наибольшее:  {Numbers.Max()}");
+        //            Console.WriteLine($"Наименьшее:  {Numbers.Min()}");
+        //            Console.WriteLine($"Среднее:  {Numbers.Average()}");
+
+        //            Console.WriteLine();
+        //        }
+        //    }
+        //}
+    }
+}
